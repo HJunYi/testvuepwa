2f60f538bffdca40aaced530358942b492025232
HeJunyi <1210778146@qq.com>
2019-12-10 11:25:37 +0800

    测试github存放静态页面

---
+++ precache-manifest.2ad4118d279243135518239559aeba8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "538df2aa43bcb3755e39",
+    "url": "/css/app.c033186d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "151d532df70d289a996166ea9e326f4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b049dd88efbd8aaef4ba",
+    "url": "/js/about.b3297082.js"
+  },
+  {
+    "revision": "538df2aa43bcb3755e39",
+    "url": "/js/app.a253ebe1.js"
+  },
+  {
+    "revision": "c336b0013c9c88dacae9",
+    "url": "/js/chunk-vendors.5338b2fa.js"
+  },
+  {
+    "revision": "135c5b67878dc06531a1892e68bfc66b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
